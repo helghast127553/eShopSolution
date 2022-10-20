509174d371e886c5bf207efdd1e138073e30b249
helghast127553 <nghia.t6363@gmail.com>
2022-10-20 16:25:01 +0700

    develop template home for customersite

---
+++ eShopSolution.WebApp/Controllers/Components/NavAccountLinksViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace eShopSolution.WebApp.Controllers.Components
+{
+    public class NavAccountLinksViewComponent: ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(string link)
+        {
+            ViewBag.link = link;
+            return View();
+        }
+    }
+}
---
+++ eShopSolution.WebApp/Controllers/Components/SideBarAccountViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+
+namespace eShopSolution.WebApp.Controllers.Components
+{
+    public class SideBarAccountViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
